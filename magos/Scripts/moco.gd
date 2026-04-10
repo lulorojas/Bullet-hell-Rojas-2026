2027865dcd04d4badc3a238652b9336c7bf6a10d
lulorojas <lulorojas11@gmail.com>
2026-04-10 01:39:59 -0300

    trol con sus puntos de movimiento, sensor del mago y ataque de moco

---
+++ magos/Scripts/moco.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var vel = 350
+var dir = Vector2.ZERO
+
+func _physics_process(delta):
+	position += dir * vel * delta
+
+func _on_body_entered(body):
+	if body.name == "Mago" or body.is_in_group("obstaculos"):
+		queue_free()
+		
---
+++ magos/Scripts/trol.gd
@@ -0,0 +1,54 @@
+extends CharacterBody2D
+
+const MOCO = preload("res://Escenas/moco.tscn")
+
+@export var puntos: Array[Marker2D]
+var indice = 0
+var aceleracion = 120
+var mago = null
+
+func _physics_process(_delta):
+	if mago != null:
+		velocity = Vector2.ZERO
+		if $AnimatedSprite2D.animation != "Atacar":
+			$AnimatedSprite2D.play("Quieto")
+	else:
+		if puntos.size() > 0:
+			var punto_objetivo = puntos[indice].global_position
+			var direccion = punto_objetivo - global_position
+			var distancia = direccion.length() 
+			
+			direccion = direccion.normalized()
+			velocity = direccion * aceleracion
+			
+			$AnimatedSprite2D.play("Caminar")
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+			
+			if distancia < 5.0:
+				indice += 1
+				if indice >= puntos.size():
+					indice = 0
+	
+	move_and_slide()
+
+func atacar():
+	$AnimatedSprite2D.play("Atacar")
+	var moco = MOCO.instantiate()
+	moco.global_position = global_position 
+	moco.dir = global_position.direction_to(mago.global_position)
+	moco.rotation = moco.dir.angle()
+	get_parent().add_child(moco)
+
+func _on_rango_body_entered(body):
+	if body.name == "Mago":
+		mago = body
+		$TimerAtaque.start()
+
+func _on_rango_body_exited(body):
+	if body == mago:
+		mago = null
+		$TimerAtaque.stop()
+
+func _on_timer_ataque_timeout():
+	if mago != null:
+		atacar()
